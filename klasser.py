a369b4c1c11d611f4704e524745604f9075d8ace
Woffsy <wilhelm.hungnes@gmail.com>
2026-02-24 13:47:50 +0100

    La til posisjoner og start på plukk opp sau og legg fra sau

--- klasser.py
+++ klasser.py
@@ -1,20 +1,39 @@
+from __future__ import annotations
 import pygame as pg
 from konstanter import *
 
 class Spiller:
-    def __init__(self) -> None:
+    def __init__(self, startX:int, startY:int) -> None:
         self.harSau:bool = False
+        self.sau:Sau|None = None
+        
+        self.x:int = startX
+        self.y:int = startY
+        
 
-    def plukkOppSau(self) -> None:
-        if self.harSau:
-            pass
-        else:
+    def plukkOppSau(self, sau:Sau) -> None:
+        if not self.harSau:
+            self.sau = sau
             self.harSau = True
+    
+    def leggFraSau(self) -> None:
+        if self.harSau and self.sau is not None:
+            self.sau.x = self.x
+            self.sau.y = self.y
+            
+            self.sau = None
+            self.harSau = False
 
 class Spokelse:
-    def __init__(self, omrade:pg.Rect) -> None:
+    def __init__(self, omrade:pg.Rect, startX:int, startY:int) -> None:
         self.omrade = omrade
+        
+        self.x:int = startX
+        self.y:int = startY
 
 class Sau:
-    def __init__(self) -> None:
+    def __init__(self, startX:int, startY:int) -> None:
         self.iSafeOmrade:bool = False
+        
+        self.x:int = startX
+        self.y:int = startY
